7d9292d7fd1a36c47a544c7d585205f38b9eb434
Mbarbuzano <alu0101629469@ull.edu.es>
2025-12-09 09:49:21 +0000

    Esqueleto

---
+++ p11/point_cloud_main.cc
@@ -0,0 +1,21 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 11: Algoritmos Voraces
+// Autor: Marcos Barbuzano Socorro
+// Correo: alu0101629469@ull.edu.es
+// Fecha: 11/12/2025
+// Archivo: point_cloud_main.cc
+// Contenido: archivo principal de compilación del proyecto
+
+#include "point_cloud.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    ModoDeEmpleo();
+  }
+
+  return 0;
+}
